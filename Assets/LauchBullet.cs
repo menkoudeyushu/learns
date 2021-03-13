74ecb2f7d75ad1e5dbfadf474040e7e48560fe74
menkoudeyushu <arsnowyizumi@126.com>
2021-03-13 23:48:11 +0800

    no message

---
+++ Assets/LauchBullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauchBullet : MonoBehaviour
+{ 
+   //[SerializeField]
+    public float speed = 50.0f;
+
+    public Transform LauchPosition;
+    public GameObject LauchObjct;
+    private void Start()
+    {
+        this.gameObject.transform.Find("bullet").GetComponent<Rigidbody2D>().velocity = transform.right * speed * Time.deltaTime;
+    }
+    private void Update()
+    {
+        Instantiate(LauchObjct, LauchPosition.position, LauchPosition.rotation);
+    }
+}
